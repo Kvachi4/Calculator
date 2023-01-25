6601bde484212f1d30407c2ea34741f48e10a929
Lasha Kvachadze <lashagold@gmail.com>
2023-01-26 00:13:08 +0400

    Calculator
    add function

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,81 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+           listBox1.Items.Clear();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int btn1 = 1;
+            listBox1.Items.Add(btn1);  
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int btn2 = 2;
+            listBox1.Items.Add(btn2);
+        }
+
+        private void additionBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            int btn3 = 3;
+            listBox1.Items.Add(btn3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            int btn4 = 4;
+            listBox1.Items.Add(btn4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            int btn6 = 6;
+            listBox1.Items.Add(btn6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            int btn7 = 7;
+            listBox1.Items.Add(btn7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            int btn8 = 8;
+            listBox1.Items.Add(btn8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            int btn9 = 9;
+            listBox1.Items.Add(btn9);
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            int btnZero = 0;
+            listBox1.Items.Add(btnZero);
+        }
+
+       
     }
 }
